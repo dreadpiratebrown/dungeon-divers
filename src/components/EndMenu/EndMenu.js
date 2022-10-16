29c3a0f78aac93043b6615b851ace9055276f638
joshuadbrown <dreadpiratebrown@gmail.com>
2022-10-16 17:01:19 -0400

    Fix styles and add icon

--- src/components/EndMenu/EndMenu.js
+++ src/components/EndMenu/EndMenu.js
@@ -3,6 +3,7 @@ import styles from "./styles.module.css";
 export const EndMenu = ({ winner, onStartClick }) => {
   return (
     <div className={styles.main}>
+      <img src="assets/tombstone.png" alt="tombstone" />
       <h1>You have died. The dungeon claims another victim.</h1>
       <button className={styles.startButton} onClick={onStartClick}>
         Play Again
