9720f18ba53273620ae2a3b0dd7c4c89bc9b5485
joshuadbrown <dreadpiratebrown@gmail.com>
2023-03-12 19:02:37 -0400

    Fix test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,6 +1,12 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
 import { App } from "./App";
+import store from "app/store";
 
 test("renders the landing page", () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
